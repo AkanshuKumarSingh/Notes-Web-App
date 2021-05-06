f4d351818e3d94463d67a02960d2ff9fccc31503
Akanshu Kumar Singh <singhakanshu7@gmail.com>
2021-05-06 21:12:21 +0530

    manu functionalities added

--- script.js
+++ script.js
@@ -11,28 +11,36 @@ for (let i = 0; i < filterColor.length; i++) {
         let mainClasses = mainContainer.getAttribute("class");
         let mainCArr = mainClasses.split(" ");
         mainCArr[1] = color;
-         mainClasses = mainCArr.join(" ");
-        mainContainer.setAttribute("class",mainClasses);
+        mainClasses = mainCArr.join(" ");
+        mainContainer.setAttribute("class", mainClasses);
 
     })
 }
 
-plusBtn.addEventListener("click",function () {
+plusBtn.addEventListener("click", function () {
     // let task = prompt("Enter Your Task");
     // let color = prompt("Color");
     let taskContainer = document.createElement("div");
-    taskContainer.setAttribute("class","ticket_container");
-    if(task != "" && color != ""){
-        let taskContainer = document.createElement("div");
-        taskContainer.setAttribute("class","ticket_container");
-        taskContainer.innerHTML = `
-        <div class="ticket_color ${color}"></div>
-        <div class="ticket_desc_conatiner">
-            <div class="ticket_id">#ExampleId</div>
-            <div class="ticket_desc">${task}</div>
+    taskContainer.setAttribute("class", "input_text");
+    taskContainer.innerHTML = `
+        <div class="ticket_desc_conatiner in">
+        <div class="ticket_id">#ExampleId</div>
+        <div class="ticket_desc">Example Text</div>
         </div>
+        <div class="color_panel"></div>
     `;
-        mainContainer.appendChild(taskContainer);
-    }
+    mainContainer.appendChild(taskContainer);
+    // if (task != "" && color != "") {
+    //     let taskContainer = document.createElement("div");
+    //     taskContainer.setAttribute("class", "ticket_container");
+    //     taskContainer.innerHTML = `
+    //     <div class="ticket_color ${color}"></div>
+    //     <div class="ticket_desc_conatiner">
+    //         <div class="ticket_id">#ExampleId</div>
+    //         <div class="ticket_desc">${task}</div>
+    //     </div>
+    // `;
+    //     mainContainer.appendChild(taskContainer);
+    // }
 
 })
