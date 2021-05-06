cdff09851febc4ecc2fb436bbacdd00bc0847a2b
Akanshu Kumar Singh <singhakanshu7@gmail.com>
2021-05-06 15:09:47 +0530

    input card made

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 let filterColor = document.querySelectorAll(".filter");
 let mainContainer = document.querySelector(".main-container");
+let plusBtn = document.querySelector(".plus");
 
-for (let i = 0; i < array.length; i++) {
-    filterColor[i].addEventListener("click",function (){
-        
+for (let i = 0; i < filterColor.length; i++) {
+    filterColor[i].addEventListener("click", function () {
         let classes = filterColor[i].getAttribute("class");
+        // console.log(classes);
         let strArr = classes.split(" ");
         let color = strArr[1];
         let mainClasses = mainContainer.getAttribute("class");
-        let mainCArr = classes.split(" ");
+        let mainCArr = mainClasses.split(" ");
         mainCArr[1] = color;
-        mainClasses = mainCArr.join(" ");
+         mainClasses = mainCArr.join(" ");
         mainContainer.setAttribute("class",mainClasses);
-    });
+
+    })
 }
 
+plusBtn.addEventListener("click",function () {
+    // let task = prompt("Enter Your Task");
+    // let color = prompt("Color");
+    let taskContainer = document.createElement("div");
+    taskContainer.setAttribute("class","ticket_container");
+    if(task != "" && color != ""){
+        let taskContainer = document.createElement("div");
+        taskContainer.setAttribute("class","ticket_container");
+        taskContainer.innerHTML = `
+        <div class="ticket_color ${color}"></div>
+        <div class="ticket_desc_conatiner">
+            <div class="ticket_id">#ExampleId</div>
+            <div class="ticket_desc">${task}</div>
+        </div>
+    `;
+        mainContainer.appendChild(taskContainer);
+    }
+
+})
